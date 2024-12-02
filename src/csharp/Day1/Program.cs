f5d58953eb02d25f02e866de12d1c41da378ee16
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-02 09:34:36 -0500

    Add editorconfig

--- src/csharp/Day1/Program.cs
+++ src/csharp/Day1/Program.cs
@@ -2,28 +2,23 @@
 
 namespace Day1;
 
-internal static class Program
-{
-    internal static void Main(string[] args)
-    {
+internal static class Program {
+    internal static void Main(string[] args) {
         var (ids1, ids2) = ReadIds();
 
         var occurrences = new Dictionary<int, int>();
-        foreach (var id in ids2)
-        {
+        foreach (var id in ids2) {
             CollectionsMarshal.GetValueRefOrAddDefault(occurrences, id, out _)++;
         }
 
         var sum = 0;
         var similarity = 0;
-        for (var i = 0; i < ids1.Count; i++)
-        {
+        for (var i = 0; i < ids1.Count; i++) {
             var id1 = ids1[i];
             var id2 = ids2[i];
 
             sum += Math.Abs(id1 - id2);
-            if (occurrences.TryGetValue(id1, out var value))
-            {
+            if (occurrences.TryGetValue(id1, out var value)) {
                 similarity += id1 * value;
             }
         }
@@ -38,8 +33,7 @@ internal static class Program
         var ids1 = new List<int>();
         var ids2 = new List<int>();
 
-        while (sr.ReadLine() is { } line)
-        {
+        while (sr.ReadLine() is { } line) {
             ids1.Add(int.Parse(line.AsSpan(0, line.IndexOf(' '))));
             ids2.Add(int.Parse(line.AsSpan(line.LastIndexOf(' '))));
         }
--- src/csharp/Day2/Program.cs
+++ src/csharp/Day2/Program.cs
@@ -6,18 +6,17 @@ internal static class Program {
         using var sr = new StreamReader(fs);
 
         var safe = 0;
-        while (sr.ReadLine() is { } line)
-        {
+        while (sr.ReadLine() is { } line) {
             var numbers = line.Split(' ').Select(int.Parse).ToArray();
 
-            if (CheckSafety(numbers, ref safe)) continue;
+            if (CheckSafety(numbers, ref safe)) {
+                continue;
+            }
 
-            for (var i = 0; i < numbers.Length; i++)
-            {
+            for (var i = 0; i < numbers.Length; i++) {
                 var numbersList = numbers.ToList();
                 numbersList.RemoveAt(i);
-                if (CheckSafety(numbersList.ToArray(), ref safe))
-                {
+                if (CheckSafety(numbersList.ToArray(), ref safe)) {
                     break;
                 }
             }
@@ -26,19 +25,15 @@ internal static class Program {
         Console.WriteLine(safe);
     }
 
-    private static bool CheckSafety(int[] numbers, ref int safe)
-    {
+    private static bool CheckSafety(int[] numbers, ref int safe) {
         var increasing = false;
         var decreasing = false;
-        for (var i = 0; i < numbers.Length - 1; i++)
-        {
-            var safety = CheckSafetyInner(numbers[i], numbers[i + 1], ref increasing, ref decreasing);
-
-            if (!safety)
+        for (var i = 0; i < numbers.Length - 1; i++) {
+            if (!CheckSafetyInner(numbers[i], numbers[i + 1], ref increasing, ref decreasing)) {
                 return false;
+            }
 
-            if (i + 1 == numbers.Length - 1)
-            {
+            if (i + 1 == numbers.Length - 1) {
                 safe++;
                 return true;
             }
@@ -47,22 +42,28 @@ internal static class Program {
         return false;
     }
 
-    private static bool CheckSafetyInner(int a, int b, ref bool increasing, ref bool decreasing)
-    {
-        if (a == b)
+    private static bool CheckSafetyInner(int a, int b, ref bool increasing, ref bool decreasing) {
+        if (a == b) {
             return false;
+        }
 
-        if (Math.Abs(a - b) > 3)
+        if (Math.Abs(a - b) > 3) {
             return false;
+        }
 
         var localIncreasing = increasing;
-        if (!increasing && b > a) localIncreasing = true;
+        if (!increasing && b > a) {
+            localIncreasing = true;
+        }
 
         var localDecreasing = decreasing;
-        if (!decreasing && b < a) localDecreasing = true;
+        if (!decreasing && b < a) {
+            localDecreasing = true;
+        }
 
-        if (localIncreasing && localDecreasing)
+        if (localIncreasing && localDecreasing) {
             return false;
+        }
 
         increasing = localIncreasing;
         decreasing = localDecreasing;
