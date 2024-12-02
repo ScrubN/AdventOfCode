1defc255d0f1b175f2ef6c5b3968cae47f0e96d3
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-02 00:35:42 -0500

    Day 2 part 2 completed

--- src/csharp/Day2/Program.cs
+++ src/csharp/Day2/Program.cs
@@ -10,29 +10,62 @@ internal static class Program {
         {
             var numbers = line.Split(' ').Select(int.Parse).ToArray();
 
-            var increasing = false;
-            var decreasing = false;
-            for (var i = 0; i < numbers.Length - 1; i++)
+            if (CheckSafety(numbers, ref safe)) continue;
+
+            for (var i = 0; i < numbers.Length; i++)
             {
-                var a = numbers[i];
-                var b = numbers[i + 1];
-
-                if (a == b) break;
-
-                if (!increasing && b > a) increasing = true;
-                if (!decreasing && b < a) decreasing = true;
-
-                if (increasing && decreasing) break;
-
-                if (Math.Abs(a - b) > 3) break;
-
-                if (i + 1 == numbers.Length - 1)
+                var numbersList = numbers.ToList();
+                numbersList.RemoveAt(i);
+                if (CheckSafety(numbersList.ToArray(), ref safe))
                 {
-                    safe++;
+                    break;
                 }
             }
         }
 
         Console.WriteLine(safe);
+    }
+
+    private static bool CheckSafety(int[] numbers, ref int safe)
+    {
+        var increasing = false;
+        var decreasing = false;
+        for (var i = 0; i < numbers.Length - 1; i++)
+        {
+            var safety = CheckSafetyInner(numbers[i], numbers[i + 1], ref increasing, ref decreasing);
+
+            if (!safety)
+                return false;
+
+            if (i + 1 == numbers.Length - 1)
+            {
+                safe++;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool CheckSafetyInner(int a, int b, ref bool increasing, ref bool decreasing)
+    {
+        if (a == b)
+            return false;
+
+        if (Math.Abs(a - b) > 3)
+            return false;
+
+        var localIncreasing = increasing;
+        if (!increasing && b > a) localIncreasing = true;
+
+        var localDecreasing = decreasing;
+        if (!decreasing && b < a) localDecreasing = true;
+
+        if (localIncreasing && localDecreasing)
+            return false;
+
+        increasing = localIncreasing;
+        decreasing = localDecreasing;
+        return true;
     }
 }
