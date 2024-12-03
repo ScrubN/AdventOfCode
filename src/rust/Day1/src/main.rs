6f31000f83ea9903b7ef002f2b5a2422e84663fc
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-02 21:18:59 -0500

    Day 1 part 2, but rust

--- src/rust/Day1/src/main.rs
+++ src/rust/Day1/src/main.rs
@@ -1,27 +1,50 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let (ids_1, ids_2) = get_ids();
 
+    let (sum, similarity) = get_sum_and_similarity(ids_1, ids_2);
+
+    println!("Sum: {} Similarity: {}", sum, similarity);
+}
+
+fn get_sum_and_similarity(ids_1: Vec<u32>, ids_2: Vec<u32>) -> (u32, u32) {
+    let mut occurrences = HashMap::<u32, u32>::new();
+    for id in &ids_2 {
+        if let Some(val) = occurrences.get_mut(id) {
+            *val += 1;
+        } else {
+            occurrences.insert(*id, 1);
+        }
+    }
+
     let mut sum = 0;
+    let mut similarity = 0;
     let mut i = 0;
     while i < ids_1.len() {
-        let diff = ids_1[i].abs_diff(ids_2[i]);
-        sum += diff;
+        let id1 = ids_1[i];
+        let id2 = ids_2[i];
+
+        sum += id1.abs_diff(id2);
+
+        if let Some(val) = occurrences.get(&id1) {
+            similarity += id1 * val;
+        }
 
         i += 1;
     }
 
-    println!("Sum: {}", sum);
+    (sum, similarity)
 }
 
-fn get_ids() -> (Vec<i32>, Vec<i32>) {
+fn get_ids() -> (Vec<u32>, Vec<u32>) {
     let file = fs::File::open("src/Inputs.txt").expect("Failed to open inputs file.");
     let reader = BufReader::new(file);
 
-    let mut ids_1: Vec<i32> = vec![];
-    let mut ids_2: Vec<i32> = vec![];
+    let mut ids_1: Vec<u32> = vec![];
+    let mut ids_2: Vec<u32> = vec![];
 
     for line in reader.lines() {
         match line {
@@ -32,8 +55,8 @@ fn get_ids() -> (Vec<i32>, Vec<i32>) {
                     .filter(|x| !x.is_empty())
                     .collect();
 
-                ids_1.push(ids_line[0].parse::<i32>().expect("Failed to parse id."));
-                ids_2.push(ids_line[1].parse::<i32>().expect("Failed to parse id."));
+                ids_1.push(ids_line[0].parse::<u32>().expect("Failed to parse id."));
+                ids_2.push(ids_line[1].parse::<u32>().expect("Failed to parse id."));
             }
             Err(_) => {
                 eprintln!("{}", line.err().unwrap());
