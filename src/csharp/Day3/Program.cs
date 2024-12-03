f8d35b54cff655f10e1e64800646484f67a442bf
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-03 00:09:06 -0500

    Day 3 part 1

--- src/csharp/Day3/Program.cs
+++ src/csharp/Day3/Program.cs
@@ -1,7 +1,20 @@
-﻿namespace Day3;
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
 
 internal static class Program {
     internal static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        var input = File.ReadAllText("Inputs.txt");
+
+        var sum = 0;
+        foreach (var match in Regex.EnumerateMatches(input, @"(?<=mul\()\d+,\d+(?=\))")) {
+            var text = input.Substring(match.Index, match.Length);
+
+            var a = int.Parse(text.AsSpan(0, text.IndexOf(',')));
+            var b = int.Parse(text.AsSpan(text.IndexOf(',') + 1));
+            sum += a * b;
+        }
+
+        Console.WriteLine(sum);
     }
 }
