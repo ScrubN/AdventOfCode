832768806e25d549ad6eeed2ef28ca8d1bc60845
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-03 17:54:04 -0500

    Cleanup day 3 C#

--- src/csharp/Day3/Program.cs
+++ src/csharp/Day3/Program.cs
@@ -1,42 +1,34 @@
-﻿using System.Buffers;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Day3;
 
-internal static class Program {
+internal static partial class Program {
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")]
+    private static partial Regex MulRegex();
+
     internal static void Main(string[] args) {
         var input = File.ReadAllText("Inputs.txt");
 
-        var searchValues = SearchValues.Create(["mul(", "do()", "don't()"], StringComparison.Ordinal);
-
-        long sum = 0;
-        var @do = true;
-        int index;
-        while ((index = input.AsSpan().IndexOfAny(searchValues)) != -1) {
-            input = input[index..];
-            if (input.StartsWith("don't()")) {
-                @do = false;
-                input = input[7..];
+        var sum = 0;
+        var enabled = true;
+        foreach (Match match in MulRegex().Matches(input)) {
+            if (match.ValueSpan.StartsWith("don't()")) {
+                enabled = false;
                 continue;
             }
 
-            if (input.StartsWith("do()")) {
-                @do = true;
-                input = input[4..];
+            if (match.ValueSpan.StartsWith("do()")) {
+                enabled = true;
                 continue;
             }
 
-            var match = Regex.Match(input, @"^mul\((\d+),(\d+)\)");
-            if (@do && match.Success) {
-                var a = int.Parse(match.Groups[1].Value);
-                var b = int.Parse(match.Groups[2].Value);
-                sum += a * b;
-
-                input = input[match.Length..];
+            if (!enabled) {
                 continue;
             }
 
-            input = input[1..];
+            var a = int.Parse(match.Groups[1].ValueSpan);
+            var b = int.Parse(match.Groups[2].ValueSpan);
+            sum += a * b;
         }
 
         Console.WriteLine(sum);
