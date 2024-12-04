d500df32061f179d746c81cddc25a7e6ea596b6e
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-03 19:01:03 -0500

    Day 3 part 2, but rust

--- src/rust/Day3/src/main.rs
+++ src/rust/Day3/src/main.rs
@@ -5,10 +5,25 @@ use std::path::PathBuf;
 fn main() {
     let file_path: PathBuf = [".", "src", "Inputs.txt"].iter().collect();
     let input = fs::read_to_string(file_path).expect("Failed to read input");
-    let reg = Regex::new(r"mul\((\d+),(\d+)\)").expect("Failed to create regex");
+    let reg = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").expect("Failed to create regex");
 
     let mut sum = 0;
+    let mut enabled = true;
     for cap in reg.captures_iter(input.as_str()) {
+        if cap.get(0).unwrap().as_str().starts_with("do()") {
+            enabled = true;
+            continue;
+        }
+
+        if cap.get(0).unwrap().as_str().starts_with("don't()") {
+            enabled = false;
+            continue;
+        }
+
+        if !enabled {
+            continue;
+        }
+
         let a = cap
             .get(1)
             .expect("Failed to capture num")
