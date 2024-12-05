f7fe22fe6a3d9ee2e6238c22f84dce7f1865fc5c
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-05 01:14:58 -0500

    Day 5 part 1

--- src/csharp/Day5/Program.cs
+++ src/csharp/Day5/Program.cs
@@ -1,7 +1,66 @@
-﻿namespace Day5;
+﻿using System.Runtime.InteropServices;
+
+namespace Day5;
 
 internal static class Program {
     internal static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        var (pageOrdering, manuals) = GetInput();
+        List<byte[]> validManuals = [];
+
+        foreach (var pageNumbers in manuals) {
+            var numbers = new HashSet<byte>(pageNumbers);
+            var orderingSlice = pageOrdering
+                .Where(x => numbers.Contains(x.Key))
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            var isValid = true;
+            HashSet<byte> validated = [];
+            foreach (var number in pageNumbers) {
+                if (orderingSlice
+                    .Where(x => !validated.Contains(x.Key))
+                    .All(x => !x.Value.Contains(number))) {
+                    validated.Add(number);
+                }
+                else {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid) {
+                validManuals.Add(pageNumbers);
+            }
+        }
+
+        var sum = 0;
+        foreach (var manual in validManuals) {
+            sum += manual[manual.Length / 2];
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static (Dictionary<byte, HashSet<byte>> pageOrdering, List<byte[]> manuals) GetInput() {
+        using var reader = new StreamReader("Inputs.txt");
+
+        var pageOrdering = new Dictionary<byte, HashSet<byte>>();
+        while (reader.ReadLine() is { } line) {
+            if (line.Length == 0) {
+                break;
+            }
+
+            var a = byte.Parse(line.AsSpan(0, line.IndexOf('|')));
+            var b = byte.Parse(line.AsSpan(line.IndexOf('|') + 1));
+            ref var set = ref CollectionsMarshal.GetValueRefOrAddDefault(pageOrdering, a, out _);
+            set ??= [];
+            set.Add(b);
+        }
+
+        var manuals = new List<byte[]>();
+        while (reader.ReadLine() is { } line) {
+            manuals.Add(line.Split(',').Select(byte.Parse).ToArray());
+        }
+
+        return (pageOrdering, manuals);
     }
 }
