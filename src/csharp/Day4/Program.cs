a8afe5f36034a5fae8701d38397c5b8d2fe6c304
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 15:52:15 -0500

    Cleanup day 4

--- src/csharp/Day4/Program.cs
+++ src/csharp/Day4/Program.cs
@@ -4,6 +4,63 @@ internal static class Program {
     internal static void Main(string[] args) {
         var input = File.ReadAllLines("Inputs.txt");
 
+        var xmasCount1 = Part1(input);
+        var xmasCount2 = Part2(input);
+
+        Console.WriteLine($"Part 1: {xmasCount1}");
+        Console.WriteLine($"Part 2: {xmasCount2}");
+    }
+
+    private static int Part1(string[] input) {
+        var xmasCount = 0;
+
+        var yLen = input.Length;
+        for (var y = 0; y < yLen; y++) {
+            var line = input[y];
+            var xLen = line.Length;
+            for (var x = 0; x < xLen; x++) {
+                if (line[x] != 'X') {
+                    continue;
+                }
+
+                if (line.AsSpan(x).StartsWith("XMAS")) {
+                    xmasCount++;
+                }
+
+                if (line.AsSpan(0, x + 1).EndsWith("SAMX")) {
+                    xmasCount++;
+                }
+
+                if (input.Up("XMAS", x, y)) {
+                    xmasCount++;
+                }
+
+                if (input.UpLeft("XMAS", x, y)) {
+                    xmasCount++;
+                }
+
+                if (input.UpRight("XMAS", x, y)) {
+                    xmasCount++;
+                }
+
+                if (input.Down("XMAS", x, y)) {
+                    xmasCount++;
+                }
+
+                if (input.DownLeft("XMAS", x, y)) {
+                    xmasCount++;
+                }
+
+                if (input.DownRight("XMAS", x, y)) {
+                    xmasCount++;
+                }
+            }
+        }
+
+        return xmasCount;
+    }
+
+    private static int Part2(string[] input) {
         var xmasCount = 0;
 
         var yLen = input.Length;
@@ -17,7 +74,7 @@ internal static class Program {
             }
         }
 
-        Console.WriteLine(xmasCount);
+        return xmasCount;
     }
 
     private static bool IsX_Mas(string[] input, int x, int y) {
@@ -29,12 +86,8 @@ internal static class Program {
             return false;
         }
 
-        if ((input.UpLeft("MAS", x + 1, y + 1) || input.UpLeft("SAM", x + 1, y + 1))
-            && (input.UpRight("MAS", x - 1, y + 1) || input.UpRight("SAM", x - 1, y + 1))) {
-            return true;
-        }
-
-        return false;
+        return (input.UpLeft("MAS", x + 1, y + 1) || input.UpLeft("SAM", x + 1, y + 1))
+               && (input.UpRight("MAS", x - 1, y + 1) || input.UpRight("SAM", x - 1, y + 1));
     }
 
     private static bool UpLeft(this string[] s, string pattern, int x, int y) {
