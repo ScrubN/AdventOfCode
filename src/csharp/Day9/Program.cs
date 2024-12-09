31b4b0883a30d6865a6d2aa72145a3311733603c
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 00:45:24 -0500

    Day 9 part 1

--- src/csharp/Day9/Program.cs
+++ src/csharp/Day9/Program.cs
@@ -2,6 +2,57 @@
 
 internal static class Program {
     internal static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        var input = File.ReadAllText("Inputs.txt").Select(x => int.Parse(x.ToString())).ToArray();
+
+        var disk = new List<int>();
+        for (var i = 0; i < input.Length; i++) {
+            var id = i % 2 == 0
+                ? i / 2
+                : -1;
+
+            for (var j = 0; j < input[i]; j++) {
+                disk.Add(id);
+            }
+        }
+
+        for (var i = disk.Count - 1; i >= 0; i--) {
+            if (disk[i] == -1) {
+                continue;
+            }
+
+            for (var j = 0; j < disk.Count; j++) {
+                if (disk[j] != -1) {
+                    continue;
+                }
+
+                disk[j] = disk[i];
+                disk[i] = -1;
+            }
+
+            var done = true;
+            foreach (var item in disk.SkipWhile(x => x != -1)) {
+                if (item != -1) {
+                    done = false;
+                }
+            }
+
+            if (done) {
+                break;
+            }
+        }
+
+        var checksum = ComputeChecksum(disk);
+
+        Console.WriteLine(checksum);
+    }
+
+    private static long ComputeChecksum(IEnumerable<int> disk) {
+        var checksum = 0L;
+        foreach (var (i, item) in disk.Index().Where(x => x.Item != -1)) {
+            var val = item * i;
+            checksum += val;
+        }
+
+        return checksum;
     }
 }
