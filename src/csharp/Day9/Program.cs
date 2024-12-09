d5730cc6c5ce349a45970c71e1c261d9dcc1528b
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 11:35:26 -0500

    Significantly faster 9 solutions

--- src/csharp/Day9/Program.cs
+++ src/csharp/Day9/Program.cs
@@ -4,7 +4,29 @@ internal static class Program {
     internal static void Main(string[] args) {
         var input = File.ReadAllText("Inputs.txt").Select(ToInt).ToArray();
 
-        var disk = new List<int>();
+        var disk1 = InitializeDisk(input);
+        var disk2 = disk1.ToList();
+
+        Part1(disk1);
+        Part2(disk2);
+
+        var checksum1 = ComputeChecksum(disk1);
+        var checksum2 = ComputeChecksum(disk2);
+
+        Console.WriteLine($"Part 1: {checksum1}");
+        Console.WriteLine($"Part 2: {checksum2}");
+    }
+
+    private static int ToInt(char arg) {
+        if (arg is >= '0' and <= '9') {
+            return arg - '0';
+        }
+
+        throw new ArgumentException(null, nameof(arg));
+    }
+
+    private static List<int> InitializeDisk(int[] input) {
+        var disk = new List<int>(input.Length);
         for (var i = 0; i < input.Length; i++) {
             var id = i % 2 == 0
                 ? i / 2
@@ -15,19 +37,45 @@ internal static class Program {
             }
         }
 
+        return disk;
+    }
+
+    private static void Part1(List<int> disk) {
         var firstEmpty = 0;
         for (var i = disk.Count - 1; i >= firstEmpty; i--) {
             if (disk[i] == -1) {
                 continue;
             }
 
+            for (var j = firstEmpty; j < i; j++) {
+                if (disk[j] != -1) {
+                    continue;
+                }
+
+                disk[j] = disk[i];
+                disk[i] = -1;
+
+                firstEmpty = j;
+                break;
+            }
+        }
+    }
+
+    private static void Part2(List<int> disk) {
+        var firstEmpty = 0;
+        for (var i = disk.Count - 1; i >= firstEmpty; i--) {
+            if (disk[i] == -1) {
+                continue;
+            }
+
+            // Compute file size
             var fileSize = 1;
             while (i - fileSize >= 0
                    && disk[i - fileSize] == disk[i]) {
                 fileSize++;
             }
 
-            firstEmpty = disk.IndexOf(-1);
+            // Search for chunk to move to
             for (var j = firstEmpty;; j++) {
                 if (j >= i) {
                     i -= fileSize - 1;
@@ -38,6 +86,7 @@ internal static class Program {
                     continue;
                 }
 
+                // Compute chunk size
                 var space = 1;
                 while (space < fileSize
                        && j + space < i
@@ -49,29 +98,31 @@ internal static class Program {
                     continue;
                 }
 
+                // Move file
+                var copied = false;
                 for (var h = 0; h < fileSize; h++) {
                     disk[j + h] = disk[i - h];
                     disk[i - h] = -1;
+                    copied = true;
+                }
+
+                if (copied) {
+                    i -= fileSize - 1;
+                    firstEmpty = disk.IndexOf(-1);
+                    break;
                 }
             }
         }
-
-        var checksum = ComputeChecksum(disk);
-
-        Console.WriteLine(checksum);
     }
 
-    private static int ToInt(char arg) {
-        if (arg is >= '0' and <= '9') {
-            return arg - '0';
-        }
-
-        throw new ArgumentException(null, nameof(arg));
-    }
-
-    private static long ComputeChecksum(IEnumerable<int> disk) {
+    private static long ComputeChecksum(List<int> disk) {
         var checksum = 0L;
-        foreach (var (i, item) in disk.Index().Where(x => x.Item != -1)) {
+        for (var i = 0; i < disk.Count; i++) {
+            var item = disk[i];
+            if (item == -1) {
+                continue;
+            }
+
             var val = item * i;
             checksum += val;
         }
