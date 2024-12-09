82c25fc4647bf2916684d02705cb5b22425c7520
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 16:05:32 -0500

    Cleanup day 3

--- src/csharp/Day3/Program.cs
+++ src/csharp/Day3/Program.cs
@@ -3,15 +3,37 @@
 namespace Day3;
 
 internal static partial class Program {
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)")]
+    private static partial Regex Part1Regex { get; }
+
     [GeneratedRegex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")]
-    private static partial Regex MulRegex();
+    private static partial Regex Part2Regex { get; }
 
     internal static void Main(string[] args) {
         var input = File.ReadAllText("Inputs.txt");
 
+        var sum1 = Part1(input);
+        var sum2 = Part2(input);
+
+        Console.WriteLine($"Part 1: {sum1}");
+        Console.WriteLine($"Part 2: {sum2}");
+    }
+
+    private static int Part1(string input) {
+        var sum = 0;
+        foreach (Match match in Part1Regex.Matches(input)) {
+            var a = int.Parse(match.Groups[1].ValueSpan);
+            var b = int.Parse(match.Groups[2].ValueSpan);
+            sum += a * b;
+        }
+
+        return sum;
+    }
+
+    private static int Part2(string input) {
         var sum = 0;
         var enabled = true;
-        foreach (Match match in MulRegex().Matches(input)) {
+        foreach (Match match in Part2Regex.Matches(input)) {
             if (match.ValueSpan.StartsWith("don't()")) {
                 enabled = false;
                 continue;
@@ -31,6 +53,6 @@ internal static partial class Program {
             sum += a * b;
         }
 
-        Console.WriteLine(sum);
+        return sum;
     }
 }
