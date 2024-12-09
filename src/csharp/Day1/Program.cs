06b36c4f9e0fe8d43a915f3fa4c396246a0bbfaa
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 16:19:26 -0500

    Cleanup day 1

--- src/csharp/Day1/Program.cs
+++ src/csharp/Day1/Program.cs
@@ -4,35 +4,24 @@ namespace Day1;
 
 internal static class Program {
     internal static void Main(string[] args) {
-        var (ids1, ids2) = ReadIds();
+        var (ids1, ids2) = GetIds();
 
         var occurrences = new Dictionary<int, int>();
         foreach (var id in ids2) {
             CollectionsMarshal.GetValueRefOrAddDefault(occurrences, id, out _)++;
         }
 
-        var sum = 0;
-        var similarity = 0;
-        for (var i = 0; i < ids1.Count; i++) {
-            var id1 = ids1[i];
-            var id2 = ids2[i];
+        var (distance, similarity) = GetIdMetrics(ids1, ids2, occurrences);
 
-            sum += Math.Abs(id1 - id2);
-            if (occurrences.TryGetValue(id1, out var value)) {
-                similarity += id1 * value;
-            }
-        }
-
-        Console.WriteLine($"Sum: {sum} Similarity: {similarity}");
+        Console.WriteLine($"Part 1: {distance}");
+        Console.WriteLine($"Part 2: {similarity}");
     }
 
-    private static (IReadOnlyList<int>, IReadOnlyList<int>) ReadIds() {
-        using var fs = File.OpenRead("Inputs.txt");
-        using var sr = new StreamReader(fs);
-
+    private static (List<int>, List<int>) GetIds() {
         var ids1 = new List<int>();
         var ids2 = new List<int>();
 
+        using var sr = new StreamReader("Inputs.txt");
         while (sr.ReadLine() is { } line) {
             ids1.Add(int.Parse(line.AsSpan(0, line.IndexOf(' '))));
             ids2.Add(int.Parse(line.AsSpan(line.LastIndexOf(' '))));
@@ -42,5 +31,21 @@ internal static class Program {
         ids2.Sort();
 
         return (ids1, ids2);
+    }
+
+    private static (int distance, int similarity) GetIdMetrics(List<int> ids1, List<int> ids2, Dictionary<int, int> occurrences) {
+        var distance = 0;
+        var similarity = 0;
+        for (var i = 0; i < ids1.Count; i++) {
+            var id1 = ids1[i];
+            var id2 = ids2[i];
+
+            distance += Math.Abs(id1 - id2);
+            if (occurrences.TryGetValue(id1, out var value)) {
+                similarity += id1 * value;
+            }
+        }
+
+        return (distance, similarity);
     }
 }
