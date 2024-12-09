e6d5ddf0b06c0013a0731d1a6a7d018ed02824d4
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-09 13:10:50 -0500

    Cleanup day 7

--- src/csharp/Day7/Program.cs
+++ src/csharp/Day7/Program.cs
@@ -4,25 +4,66 @@ internal static class Program {
     internal static void Main(string[] args) {
         var input = File.ReadAllLines("Inputs.txt");
 
-        var sum = input.Sum(Compute);
+        var sum1 = 0L;
+        foreach (var line in input) {
+            sum1 += Part1(line);
+        }
 
-        Console.WriteLine(sum);
+        var sum2 = 0L;
+        foreach (var line in input) {
+            sum2 += Part2(line);
+        }
+
+        Console.WriteLine($"Part 1: {sum1}");
+        Console.WriteLine($"Part 2: {sum2}");
     }
 
-    private static long Compute(string line) {
-        var expected = long.Parse(line.AsSpan(0, line.IndexOf(':')));
-        var parameters = line[(line.IndexOf(':') + 1)..]
-            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-            .Select(long.Parse)
-            .ToArray();
+    private static long Part1(string line) {
+        var (expected, parameters) = ParseLine(line);
 
-        for (var mask = 0L; mask < Math.Pow(3, parameters.Length); mask++) {
+        var maskMax = Math.Pow(2, parameters.Length); // Removing from the loop prevents it from being recomputed every iteration
+        for (var mask = 0L; mask < maskMax; mask++) {
             var runningTotal = parameters[0];
+
             var workingMask = mask;
             for (var i = 1; i < parameters.Length; i++) {
                 var param = parameters[i];
-                var op = workingMask % 3;
-                switch (op) {
+
+                if ((workingMask & 1) == 1) {
+                    runningTotal *= param;
+                }
+                else {
+                    runningTotal += param;
+                }
+
+                // Strangely, this makes it 5-10% slower
+                // if (runningTotal > expected) {
+                //     break;
+                // }
+
+                workingMask >>= 1;
+            }
+
+            if (runningTotal == expected) {
+                return expected;
+            }
+        }
+
+        return 0;
+    }
+
+    private static long Part2(string line) {
+        var (expected, parameters) = ParseLine(line);
+
+        var maskMax = Math.Pow(3, parameters.Length); // Removing from the loop prevents it from being recomputed every iteration
+        for (var mask = 0L; mask < maskMax; mask++) {
+            var runningTotal = parameters[0];
+
+            var workingMask = mask;
+            for (var i = 1; i < parameters.Length; i++) {
+                var param = parameters[i];
+
+                switch (workingMask % 3) {
                     case 0:
                         runningTotal += param;
                         break;
@@ -30,8 +71,18 @@ internal static class Program {
                         runningTotal *= param;
                         break;
                     case 2:
-                        runningTotal = runningTotal * (long)Math.Pow(10, CountDigits(param)) + param;
+                        // For loop is faster than Math.Pow
+                        var digits = CountDigits(param); // Removing from the loop prevents it from being recomputed every iteration
+                        for (var j = 0; j < digits; j++) {
+                            runningTotal *= 10;
+                        }
+
+                        runningTotal += param;
                         break;
+                }
+
+                if (runningTotal > expected) {
+                    break;
                 }
 
                 workingMask /= 3;
@@ -45,6 +96,17 @@ internal static class Program {
         }
 
         return 0;
+    }
+
+    private static (long, long[]) ParseLine(string line) {
+        var colonIndex = line.IndexOf(':');
+        var expected = long.Parse(line.AsSpan(0, colonIndex));
+        var parameters = line[(colonIndex + 1)..]
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToArray();
+
+        return (expected, parameters);
     }
 
     private static int CountDigits(long number) {
