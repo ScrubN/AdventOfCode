cd58d58017ce1991dd85b26743126c0396791ba7
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-10 18:48:14 -0500

    Day 10 part 2

--- src/csharp/Day10/Program.cs
+++ src/csharp/Day10/Program.cs
@@ -7,8 +7,10 @@ internal static class Program {
         var input = File.ReadAllLines("Inputs.txt").Select(x => x.Select(ToInt).ToArray()).ToArray();
 
         var sum1 = Part1(input);
+        var sum2 = Part2(input);
 
-        Console.WriteLine(sum1);
+        Console.WriteLine($"Part 1: {sum1}");
+        Console.WriteLine($"Part 2: {sum2}");
     }
 
     private static int ToInt(char arg) {
@@ -27,14 +29,14 @@ internal static class Program {
                     continue;
                 }
 
-                sum += ExploreNext(0, input, x, y);
+                sum += ExploreNext1(0, input, x, y);
             }
         }
 
         return sum;
     }
 
-    private static int ExploreNext(int i, int[][] input, int x, int y, HashSet<Point>? taken = null) {
+    private static int ExploreNext1(int i, int[][] input, int x, int y, HashSet<Point>? taken = null) {
         taken ??= [];
 
         if (i == 9) {
@@ -44,25 +46,73 @@ internal static class Program {
         var total = 0;
         if (y - 1 >= 0) {
             if (input[y - 1][x] == i + 1) {
-                total += ExploreNext(i + 1, input, x, y - 1, taken);
+                total += ExploreNext1(i + 1, input, x, y - 1, taken);
             }
         }
 
         if (y + 1 < input.Length) {
             if (input[y + 1][x] == i + 1) {
-                total += ExploreNext(i + 1, input, x, y + 1, taken);
+                total += ExploreNext1(i + 1, input, x, y + 1, taken);
             }
         }
 
         if (x - 1 >= 0) {
             if (input[y][x - 1] == i + 1) {
-                total += ExploreNext(i + 1, input, x - 1, y, taken);
+                total += ExploreNext1(i + 1, input, x - 1, y, taken);
             }
         }
 
         if (x + 1 < input[y].Length) {
             if (input[y][x + 1] == i + 1) {
-                total += ExploreNext(i + 1, input, x + 1, y, taken);
+                total += ExploreNext1(i + 1, input, x + 1, y, taken);
+            }
+        }
+
+        return total;
+    }
+
+    private static int Part2(int[][] input) {
+        var sum = 0;
+        for (var y = 0; y < input.Length; y++) {
+            for (var x = 0; x < input[y].Length; x++) {
+                if (input[y][x] != 0) {
+                    continue;
+                }
+
+                sum += ExploreNext2(0, input, x, y);
+            }
+        }
+
+        return sum;
+    }
+
+    private static int ExploreNext2(int i, int[][] input, int x, int y) {
+        if (i == 9) {
+            return 1;
+        }
+
+        var total = 0;
+        if (y - 1 >= 0) {
+            if (input[y - 1][x] == i + 1) {
+                total += ExploreNext2(i + 1, input, x, y - 1);
+            }
+        }
+
+        if (y + 1 < input.Length) {
+            if (input[y + 1][x] == i + 1) {
+                total += ExploreNext2(i + 1, input, x, y + 1);
+            }
+        }
+
+        if (x - 1 >= 0) {
+            if (input[y][x - 1] == i + 1) {
+                total += ExploreNext2(i + 1, input, x - 1, y);
+            }
+        }
+
+        if (x + 1 < input[y].Length) {
+            if (input[y][x + 1] == i + 1) {
+                total += ExploreNext2(i + 1, input, x + 1, y);
             }
         }
 
