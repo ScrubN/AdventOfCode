100b69fc305a91b4c78b5dad54a9e17e4ae85d2f
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-11 15:38:27 -0500

    Day 11 part 2

--- src/csharp/Day11/Program.cs
+++ src/csharp/Day11/Program.cs
@@ -1,25 +1,29 @@
-﻿namespace Day11;
+﻿using System.Runtime.InteropServices;
+
+namespace Day11;
 
 internal static class Program {
     internal static void Main(string[] args) {
         var input = File.ReadAllText("Inputs.txt").Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(long.Parse).ToArray();
 
         var sum1 = Part1(input);
+        var sum2 = Part2(input);
 
-        Console.WriteLine(sum1);
+        Console.WriteLine($"Part 1: {sum1}");
+        Console.WriteLine($"Part 2: {sum2}");
     }
 
     private static long Part1(long[] input) {
         var stones = input.ToList();
         for (var i = 0; i < 25; i++) {
-            stones = FindNewStones1(stones);
+            stones = FindNewStones(stones);
         }
 
         return stones.Count;
     }
 
-    private static List<long> FindNewStones1(IEnumerable<long> stones) {
-        List<long> newStones = [];
+    private static List<long> FindNewStones(List<long> stones) {
+        var newStones = new List<long>(stones.Count);
         foreach (var stone in stones) {
             if (stone == 0) {
                 newStones.Add(1);
@@ -48,6 +52,49 @@ internal static class Program {
         }
 
         return newStones;
+    }
+
+    private static long Part2(long[] input) {
+        return ObserveStones(input, 75);
+    }
+
+    private static long ObserveStones(long[] input, long iterations) {
+        Dictionary<long, long> stones = [];
+        foreach (var stone in input) {
+            stones.GetValueRefOrAddDefault(stone)++;
+        }
+
+        for (var i = 0; i < iterations; i++) {
+            Dictionary<long, long> iStones = [];
+            foreach (var (value, count) in stones) {
+                if (value == 0) {
+                    iStones.GetValueRefOrAddDefault(1) += count;
+                    continue;
+                }
+
+                var digits = CountDigits(value);
+                if (digits % 2 == 0) {
+                    var d = 1;
+                    for (var j = 0; j < digits / 2; j++) {
+                        d *= 10;
+                    }
+
+                    iStones.GetValueRefOrAddDefault(value / d) += count;
+                    iStones.GetValueRefOrAddDefault(value % d) += count;
+                    continue;
+                }
+
+                iStones.GetValueRefOrAddDefault(value * 2024) += count;
+            }
+
+            stones = iStones;
+        }
+
+        return stones.Sum(x => x.Value);
+    }
+
+    private static ref TValue? GetValueRefOrAddDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TKey : notnull {
+        return ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out _);
     }
 
     private static int CountDigits(long number) {
