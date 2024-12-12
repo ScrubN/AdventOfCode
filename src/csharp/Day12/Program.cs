a321bd717430f958a08b9c73e173e4ee133cd018
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-12 10:44:42 -0500

    Day 12 part 2

--- src/csharp/Day12/Program.cs
+++ src/csharp/Day12/Program.cs
@@ -6,9 +6,11 @@ internal static class Program {
     internal static void Main(string[] args) {
         var input = File.ReadAllLines("Inputs.txt");
 
-        var cost = Part1(input);
+        var cost1 = Part1(input);
+        var cost2 = Part2(input);
 
-        Console.WriteLine(cost);
+        Console.WriteLine($"Part 1: {cost1}");
+        Console.WriteLine($"Part 2: {cost2}");
     }
 
     private static int Part1(string[] input) {
@@ -17,73 +19,61 @@ internal static class Program {
             .ToHashSet();
 
         Dictionary<Point, int> costs = [];
-        while (points.FirstOrDefault() is { } point) {
-            var type = input[point.Y][point.X];
-            HashSet<Point> discovered = [];
-            HashSet<Point?> queue = [point];
-
-            Console.WriteLine($"{type}: {point.X}, {point.Y} - {points.Count} left.");
-
-            while (queue.FirstOrDefault() is { } pointToCheck) {
-                queue.Remove(pointToCheck);
-
-                if (input[pointToCheck.Y][pointToCheck.X] != type) {
-                    continue;
-                }
-
-                discovered.Add(pointToCheck);
-                points.Remove(pointToCheck);
-
-                for (var y = -1; y <= 1; y++) {
-                    var y2 = pointToCheck.Y + y;
-                    if (y2 < 0 || y2 >= input.Length) {
-                        continue;
-                    }
-
-                    for (var x = -1; x <= 1; x++) {
-                        var x2 = pointToCheck.X + x;
-                        if (x2 < 0 || x2 >= input[y2].Length) {
-                            continue;
-                        }
-
-                        if (Math.Abs(x) == Math.Abs(y)) {
-                            continue;
-                        }
-
-                        if (input[y2][x2] != type) {
-                            continue;
-                        }
-
-                        var newPoint = new Point(x2, y2);
-                        if (points.Contains(newPoint) && !discovered.Contains(newPoint)) {
-                            queue.Add(newPoint);
-                        }
-                    }
-                }
-            }
+        while (points.FirstOrDefault() is { } start) {
+            var discovered = FloodFill(input, points, start);
 
             var area = discovered.Count;
             var perimeter = ComputePerimeter(discovered);
-
-            costs[point] = area * perimeter;
+            costs[start] = area * perimeter;
         }
 
         return costs.Sum(x => x.Value);
     }
 
-    private static void PrintArea(string[] input, HashSet<Point> discovered) {
-        for (var y = 0; y < input.Length; y++) {
-            for (var x = 0; x < input[y].Length; x++) {
-                if (discovered.Contains(new Point(x, y))) {
-                    Console.Write(input[y][x]);
-                }
-                else {
-                    Console.Write('.');
-                }
+    private static HashSet<Point> FloodFill(string[] input, HashSet<Point?> points, Point start) {
+        var type = input[start.Y][start.X];
+        HashSet<Point> discovered = [];
+        HashSet<Point?> queue = [start];
+
+        while (queue.FirstOrDefault() is { } pointToCheck) {
+            queue.Remove(pointToCheck);
+
+            if (input[pointToCheck.Y][pointToCheck.X] != type) {
+                continue;
             }
 
-            Console.WriteLine();
+            discovered.Add(pointToCheck);
+            points.Remove(pointToCheck);
+
+            for (var y = -1; y <= 1; y++) {
+                var y2 = pointToCheck.Y + y;
+                if (y2 < 0 || y2 >= input.Length) {
+                    continue;
+                }
+
+                for (var x = -1; x <= 1; x++) {
+                    var x2 = pointToCheck.X + x;
+                    if (x2 < 0 || x2 >= input[y2].Length) {
+                        continue;
+                    }
+
+                    if (Math.Abs(x) == Math.Abs(y)) {
+                        continue;
+                    }
+
+                    if (input[y2][x2] != type) {
+                        continue;
+                    }
+
+                    var newPoint = new Point(x2, y2);
+                    if (points.Contains(newPoint) && !discovered.Contains(newPoint)) {
+                        queue.Add(newPoint);
+                    }
+                }
+            }
         }
+
+        return discovered;
     }
 
     private static int ComputePerimeter(HashSet<Point> discovered) {
@@ -107,5 +97,68 @@ internal static class Program {
         }
 
         return perimeter;
+    }
+
+    private static int Part2(string[] input) {
+        var points = input
+            .SelectMany((s, y) => s.Select((_, x) => (Point?)new Point(x, y)))
+            .ToHashSet();
+
+        Dictionary<Point, int> costs = [];
+        while (points.FirstOrDefault() is { } start) {
+            var discovered = FloodFill(input, points, start);
+
+            var area = discovered.Count;
+            var sides = ComputeSides(discovered);
+            costs[start] = area * sides;
+        }
+
+        return costs.Sum(x => x.Value);
+    }
+
+    private enum Direction {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    private static int ComputeSides(HashSet<Point> discovered) {
+        HashSet<(Point point, Direction direction)> sides = [];
+        foreach (var point in discovered) {
+            var newPoint = point with { X = point.X + 1 };
+            if (!discovered.Contains(newPoint)) {
+                sides.Add((newPoint, Direction.Right));
+            }
+
+            newPoint = point with { X = point.X - 1 };
+            if (!discovered.Contains(newPoint)) {
+                sides.Add((newPoint, Direction.Left));
+            }
+
+            newPoint = point with { Y = point.Y + 1 };
+            if (!discovered.Contains(newPoint)) {
+                sides.Add((newPoint, Direction.Down));
+            }
+
+            newPoint = point with { Y = point.Y - 1 };
+            if (!discovered.Contains(newPoint)) {
+                sides.Add((newPoint, Direction.Up));
+            }
+        }
+
+        foreach (var side in sides.OrderBy(x => x.point.X).ThenBy(x => x.point.Y).ToArray()) {
+            var newSide = side with { point = side.point with { X = side.point.X + 1 } };
+            if (sides.Contains(newSide)) {
+                sides.Remove(side);
+            }
+
+            newSide = side with { point = side.point with { Y = side.point.Y + 1 } };
+            if (sides.Contains(newSide)) {
+                sides.Remove(side);
+            }
+        }
+
+        return sides.Count;
     }
 }
