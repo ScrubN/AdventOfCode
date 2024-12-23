b3d8551c35adc13fad8d0a3c0c8c30feaaca7bcf
ScrubN <72096833+ScrubN@users.noreply.github.com>
2024-12-22 21:30:08 -0500

    Day 22 part 1

--- src/csharp/Day22/Program.cs
+++ src/csharp/Day22/Program.cs
@@ -2,6 +2,36 @@
 
 internal static class Program {
     internal static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        var input = File.ReadLines("Inputs.txt").Select(long.Parse).ToArray();
+
+        var sum1 = Part1(input);
+
+        Console.WriteLine(sum1);
     }
+
+    private static long Part1(long[] input) {
+        var sum = 0L;
+
+        foreach (var l in input) {
+            var l2 = l;
+            for (var i = 0; i < 2_000; i++) {
+                l2 = PseudoRandom(l2);
+            }
+
+            sum += l2;
+        }
+
+        return sum;
+    }
+
+    private static long PseudoRandom(long secretNum) {
+        var a = Prune(Mix(secretNum, secretNum * 64));
+        var b = Prune(Mix(a, a / 32));
+        var c = Prune(Mix(b, b * 2048));
+        return c;
+    }
+
+    private static long Mix(long secretNum, long value) => secretNum ^ value;
+
+    private static long Prune(long secretNum) => secretNum % 16777216;
 }
