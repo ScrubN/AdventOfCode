0f8273e4e00ec31eb614a9a4e9434e012ea9c631
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-12-02 10:40:57 -0500

    Day 2 part 2 complete

--- 2025/csharp/Day2/Program.cs
+++ 2025/csharp/Day2/Program.cs
@@ -8,15 +8,16 @@ internal static class Program {
     public static void Main(string[] args) {
         var data = GetIdRanges();
 
-        var part1 = SumInvalidIds(data);
+        var part1 = SumInvalidIdsPart1(data);
+        var part2 = SumInvalidIdsPart2(data);
 
         Console.WriteLine($"Part 1: {part1}");
-        // Console.WriteLine($"Part 2: {part2}");
+        Console.WriteLine($"Part 2: {part2}");
     }
 
     private static List<Range> GetIdRanges() {
         var input = File.ReadAllText("Inputs.txt")
-            .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            .Split(',');
 
         var idRanges = new List<Range>();
         foreach (var str in input) {
@@ -31,29 +32,90 @@ internal static class Program {
         return idRanges;
     }
 
-    private static long SumInvalidIds(List<Range> data) {
-        var invalidIds = new List<long>();
+    private static long SumInvalidIdsPart1(List<Range> data) {
+        var sum = 0L;
 
         foreach (var range in data) {
             for (var i = range.Start; i <= range.End; i++) {
                 var digits = CountDigits(i);
 
-                // Cannot be repeated sequence if even
-                if (digits % 2 != 0)
-                    continue;
-
-                // ReSharper disable once PossibleLossOfFraction
-                var magnitude = (long)Math.Pow(10, digits / 2);
-                var upper = i / magnitude;
-                var lower = i - upper * magnitude;
-
-                if (upper == lower) {
-                    invalidIds.Add(i);
+                if (IsInvalidIdPart1(digits, i)) {
+                    sum += i;
                 }
             }
         }
 
-        return invalidIds.Sum();
+        return sum;
+    }
+
+    private static bool IsInvalidIdPart1(int digits, long i) {
+        // Cannot be repeated sequence if even
+        if (digits % 2 != 0)
+            return false;
+
+        // ReSharper disable once PossibleLossOfFraction
+        var magnitude = (long)Math.Pow(10, digits / 2);
+        var upper = i / magnitude;
+        var lower = i - upper * magnitude;
+
+        return upper == lower;
+    }
+
+
+    private static long SumInvalidIdsPart2(List<Range> data) {
+        var digitFactors = Enumerable.Range(0, 30)
+            .Select(Factors)
+            .ToArray();
+
+        var sum = 0L;
+
+        foreach (var range in data) {
+            for (var i = range.Start; i <= range.End; i++) {
+                var digits = CountDigits(i);
+
+                if (!IsInvalidIdPart2(digits, i, digitFactors[digits])) {
+                    continue;
+                }
+
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    private static bool IsInvalidIdPart2(int digits, long id, int[] digitFactors) {
+        Span<char> str = stackalloc char[digits];
+        var success = id.TryFormat(str, out var written);
+        Debug.Assert(success);
+        Debug.Assert(str.Length == written);
+
+        foreach (var factor in digitFactors) {
+            if (HasRepeatingSequence(str, factor)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool HasRepeatingSequence(ReadOnlySpan<char> str, int divisions) {
+        Debug.Assert(str.Length % divisions == 0);
+
+        var divisionLen =  str.Length / divisions;
+
+        var last = str[..divisionLen];
+        for (var i = 1; i < divisions; i++) {
+            var start = divisionLen * i;
+            var end = start + divisionLen;
+            var current = str[start..end];
+
+            if (!last.SequenceEqual(current)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private static int CountDigits(long number) {
@@ -67,5 +129,17 @@ internal static class Program {
         }
 
         return digits;
+    }
+
+    private static int[] Factors(int number) {
+        var factors = new List<int>();
+
+        for (var i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                factors.Add(i);
+            }
+        }
+
+        return factors.ToArray();
     }
 }
