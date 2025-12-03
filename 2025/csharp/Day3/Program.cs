7f81d29983a1688c3056d5c25ccc1acc9943e6e1
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-12-03 00:48:58 -0500

    Day 3 part 1

---
+++ 2025/csharp/Day3/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Day3;
+
+internal static class Program {
+    public static void Main(string[] args) {
+        var batteries = GetBatteries();
+
+        var part1 = Part1(batteries);
+        // var part2 = Part2(batteries);
+
+        Console.WriteLine($"Part 1: {part1}");
+        // Console.WriteLine($"Part 2: {part2}");
+    }
+
+    private static List<int[]> GetBatteries() {
+        var batteries = new List<int[]>();
+
+        using var sr = new StreamReader("Inputs.txt");
+        while (sr.ReadLine() is { } line) {
+            batteries.Add(line.Select(x => int.Parse([x]))
+                .ToArray());
+        }
+
+        return batteries;
+    }
+
+    private static int Part1(List<int[]> batteries) {
+        var sum = 0;
+
+        foreach (var battery in batteries) {
+            sum += TurnOnBatteriesPart1(battery);
+        }
+
+        return sum;
+    }
+
+    private static int TurnOnBatteriesPart1(int[] battery) {
+        var max = 0;
+
+        for (var i = 0; i < battery.Length - 1; i++) {
+            var left = battery[i] * 10;
+
+            for (var j = i + 1; j < battery.Length; j++) {
+                var right = battery[j];
+
+                var sum = left + right;
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
+    }
+}
---
+++ 2025/csharp/Day4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day4;
+
+internal static class Program {
+    public static void Main(string[] args) {
+        var data = GetData();
+
+        var part1 = Part1(data);
+        // var part2 = CountZeroClicks(data);
+
+        Console.WriteLine($"Part 1: {part1}");
+        // Console.WriteLine($"Part 2: {part2}");
+    }
+
+    private static List<> GetData() {
+        var data = new List<Turn>();
+
+        using var sr = new StreamReader("Inputs.txt");
+        while (sr.ReadLine() is { } line) {
+            data.Add();
+        }
+
+        return data;
+    }
+
+    private static int Part1(object data) {
+        throw new NotImplementedException();
+    }
+}
