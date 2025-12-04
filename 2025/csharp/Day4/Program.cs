56559e945e56d1ec18dc256e5abb39f448d093ae
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-12-04 00:13:06 -0500

    Day 4 part 1

--- 2025/csharp/Day4/Program.cs
+++ 2025/csharp/Day4/Program.cs
@@ -5,24 +5,72 @@ internal static class Program {
         var data = GetData();
 
         var part1 = Part1(data);
-        // var part2 = CountZeroClicks(data);
+        // var part2 = Part2(data);
 
         Console.WriteLine($"Part 1: {part1}");
         // Console.WriteLine($"Part 2: {part2}");
     }
 
-    private static List<> GetData() {
-        var data = new List<Turn>();
+    private static bool[,] GetData() {
+        var lines = File.ReadAllLines("Inputs.txt");
+        var data = new bool[lines.Length, lines[0].Length];
 
-        using var sr = new StreamReader("Inputs.txt");
-        while (sr.ReadLine() is { } line) {
-            data.Add();
+        for (var y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                data[x, y] = line[x] == '@';
+            }
         }
 
         return data;
     }
 
-    private static int Part1(object data) {
-        throw new NotImplementedException();
+    private static int Part1(bool[,] data) {
+        var count = 0;
+
+        var yLen = data.GetLength(1);
+        var xLen = data.GetLength(0);
+        for (var y = 0; y < yLen; y++)
+        for (var x = 0; x < xLen; x++) {
+            if (!data[y, x]) {
+                continue;
+            }
+
+            if (IsValidTile(xLen, yLen, x, y, data, 3)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static bool IsValidTile(int xLen, int yLen, int x, int y, bool[,] data, int maxAdjacent) {
+        var adjacent = 0;
+
+        for (var i = -1; i <= 1 ; i++)
+        for (var j =-1; j <= 1 ; j++) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            var aX = x + i;
+            var aY = y + j;
+            if (aX < 0 || aX >= xLen || aY < 0 || aY >= yLen) {
+                continue;
+            }
+
+            if (!data[aY, aX]) {
+                continue;
+            }
+
+            adjacent++;
+            if (adjacent > maxAdjacent) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
