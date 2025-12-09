88c43323f1daebfa33ddaa66d93ad3b1a8555af3
ScrubN <72096833+ScrubN@users.noreply.github.com>
2025-12-09 10:44:02 -0500

    Day 6 part 2

--- 2025/csharp/Day6/Program.cs
+++ 2025/csharp/Day6/Program.cs
@@ -3,6 +3,10 @@
 namespace Day6;
 
 internal static class Program {
+    extension(Range range) {
+        private int Length => range.End.Value - range.Start.Value;
+    }
+
     public enum Operation {
         None,
         Add,
@@ -10,16 +14,16 @@ internal static class Program {
     }
 
     public static void Main(string[] args) {
-        var worksheet = GetWorksheet();
-
-        var part1 = Part1(worksheet);
-        // var part2 = Part2(ranges);
-
+        var worksheetPart1 = GetWorksheetPart1();
+        var part1 = SolveWorksheet(worksheetPart1);
         Console.WriteLine($"Part 1: {part1}");
-        // Console.WriteLine($"Part 2: {part2}");
+
+        var worksheetPart2 = GetWorksheetPart2();
+        var part2 = SolveWorksheet(worksheetPart2);
+        Console.WriteLine($"Part 2: {part2}");
     }
 
-    private static List<(List<int> numbers, Operation operation)> GetWorksheet() {
+    private static List<(List<int> numbers, Operation operation)> GetWorksheetPart1() {
         var worksheet = new List<(List<int> numbers, Operation operation)>();
 
         var firstRun = true;
@@ -55,12 +59,52 @@ internal static class Program {
         return worksheet;
     }
 
-    private static long Part1(List<(List<int> numbers, Operation operation)> worksheet) {
+    private static List<(List<int> numbers, Operation operation)> GetWorksheetPart2() {
+        var lines = File.ReadAllLines("Inputs.txt");
+
+        var ranges = new List<Range>();
+        var start = 0;
+        for (var i = 0; i < lines[0].Length; i++) {
+            if (lines.Any(x => x[i] != ' ')) continue;
+
+            ranges.Add(new Range(start, i));
+            start = i + 1;
+        }
+
+        ranges.Add(new Range(start, lines[0].Length));
+
+
+        var worksheet = new List<(List<int> numbers, Operation operation)>(ranges.Count);
+        Span<char> numberScratch = stackalloc char[lines.Length - 1];
+        foreach (var range in ranges) {
+            List<int> numbers = [];
+            for (var i = 0; i < range.Length; i++) {
+                numberScratch.Fill(' ');
+                for (var j = 0; j < lines.Length - 1; j++)
+                {
+                    numberScratch[j] = lines[j][range.Start.Value + i];
+                }
+
+                numbers.Add(int.Parse(numberScratch));
+            }
+
+            var operation = lines[^1].AsSpan(range).Trim()[0] switch {
+                '+' => Operation.Add,
+                '*' => Operation.Multiply,
+                _ => throw new UnreachableException()
+            };
+
+            worksheet.Add((numbers, operation));
+        }
+
+        return worksheet;
+    }
+
+    private static long SolveWorksheet(List<(List<int> numbers, Operation operation)> worksheet) {
         var sum = 0L;
 
         foreach (var (column, operation) in worksheet) {
-            switch (operation)
-            {
+            switch (operation) {
                 case Operation.Add:
                     sum += column.Sum();
                     break;
